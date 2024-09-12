d2ac28a7b7c5915ac3f3a480e91ab5108c2317eb
TheColossus <kushalchaturvedi822@gmail.com>
2024-09-11 21:30:44 -0400

    Fixed example

--- example.py
+++ example.py
@@ -1,9 +1,8 @@
 import pandas as pd
-import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
 from kernel import MLP
-from visualizer import draw_dot
 
 #Input data
 #Replace the path to the csv as needed. If you keep it like this, ensure you run the program from the 'example' directory
@@ -24,10 +23,12 @@ transformedDataset = scaler.transform(dataset)
 xTrain, xTest, yTrain, yTest = train_test_split(transformedDataset, outputRows, test_size=0.25, random_state=0)
 xTrain = xTrain.tolist()
 yTrain = yTrain.tolist()
+xTest = xTest.tolist()
+yTest = yTest.tolist()
 
 #Build the model
 #2 layer MLP with 13 dimensional inputs (The dataset has 13 input columns)
-model = MLP(13, [5, 1])
+model = MLP(13, [20, 20, 1])
 
 #Define the loss function
 def BinaryCrossEntropy(yTrue, yPred):
@@ -56,7 +57,25 @@ for epoch in range(100):
         #Update weights and biases
     for params in model.parameters():
         params.data -= 0.001 * params.grad
-    # for param in model.parameters():
-    #     print(f"Parameter: {param}, Gradient: {param.grad}")
-    print(epoch, loss.data)
+
+#Test the model
+yPredictions = [model(x) for x in xTest]
+
+#Pull the data property out of each Scalar
+yPredictionsClean = [x.data for x in yPredictions]
+
+#Push the data through a threshold to make it binary
+
+yPredictionsFinal = [0 if x < 0.5 else 1 for x in yPredictionsClean]
+print(yPredictionsFinal)
+
+cm = confusion_matrix(yTest, yPredictionsFinal)
+
+TN, FP, FN, TP = confusion_matrix(yTest, yPredictionsFinal).ravel()
+
+print('True Positive(TP)  = ', TP)
+print('False Positive(FP) = ', FP)
+print('True Negative(TN)  = ', TN)
+print('False Negative(FN) = ', FN)
+
 
